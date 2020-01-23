0fb477356118edf2a5d14ff10e0cb83841d0a4ec
Александр Брыков <abrykov@MacBook-Pro-Aleksandr.local>
2020-01-24 00:15:30 +0300

    Added model template

---
+++ src/model.js
@@ -0,0 +1,34 @@
+const model = {
+    week: 1000,
+    days: [
+        // day 1
+        [
+            {
+                component: 'EA',
+                milestone: '4.7',
+                task: '43726842',
+                hours: 4,
+                text: 'Чот фиксил'
+            },
+            {
+                component: 'EA',
+                milestone: '4.7',
+                task: '43726842',
+                hours: 4,
+                text: 'Чот исследовал'
+            },
+        ],
+        // day 2
+        [
+            {
+                component: 'EA',
+                milestone: '4.7',
+                task: '43726842',
+                hours: 8,
+                text: 'Чот снова фиксил'
+            },
+        ],
+    ]
+};
+
+export default model;
